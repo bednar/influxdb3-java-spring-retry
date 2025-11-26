15305bf42617c50f5d348d23cfd13afbe27e316f
karel rehor <karl.koerner@bonitoo.io>
2025-11-26 10:27:58 +0100

    feat: adds testing and profiling endpoints

--- src/main/java/com/example/influxretry/controller/QueryController.java
+++ src/main/java/com/example/influxretry/controller/QueryController.java
@@ -1,13 +1,27 @@
 package com.example.influxretry.controller;
 
+import ch.qos.logback.classic.Level;
+import com.example.influxretry.Influxdb3JavaSpringRetryApplication;
+import com.example.influxretry.influx.InfluxV3QueryExecutionMetricsLogger;
 import com.example.influxretry.service.QueryService;
+import com.google.j2objc.annotations.Property;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.influxdb.v3.client.PointValues;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * REST controller that exposes an endpoint for executing SQL queries.
@@ -20,7 +34,15 @@ import com.influxdb.v3.client.PointValues;
 @RequiredArgsConstructor
 public class QueryController {
 
+    //static List<PointValues> memoryHog = new ArrayList<>();
+
     private final QueryService queryService;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Value("${server.shutdown}")
+    private String shutdownType;
 
     /**
      * Execute a SQL query against InfluxDB and return the number of rows.
@@ -36,6 +58,99 @@ public class QueryController {
     }
 
     /**
+     * A simpler list of threads than the one returned by the actuator.
+     *
+     * @return list of thread information
+     */
+    @GetMapping("/threads/all")
+    public String all() {
+        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        StringBuilder sb = new StringBuilder();
+        for (Thread t : threadSet) {
+            sb.append(t.getName())
+                .append("[")
+                .append(t.getThreadGroup().getName())
+                .append("]: ")
+                .append(t.getState())
+                .append("\n");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Force a thread interrupt and inspect results in SLF4J log.
+     *
+     * @param name - name of the thread to be interrupted.
+     * @return - result of interrupt attempt, or name not found.
+     */
+    @GetMapping("/threads/interrupt")
+    public String interrupt(@RequestParam(name = "t") String name) {
+        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        for (Thread t : threadSet) {
+            if (t.getName().equals(name)) {
+                t.interrupt();
+                return "Interrupted " + name + "\n";
+            }
+        }
+        return "Not found " + name + "\n";
+    }
+
+    /**
+     * Get standard information about a thread.
+     *
+     * @param name - target thread.
+     * @return - information or not found.
+     */
+    @GetMapping("/threads/info")
+    public String info(@RequestParam(name = "t") String name) {
+        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        StringBuilder sb = new StringBuilder();
+        for (Thread t : threadSet) {
+            if (t.getName().equals(name)) {
+                return t + "\n";
+            }
+        }
+        return "Not found " + name + "\n";
+    }
+
+    /**
+     * Return a ledger of results from queries
+     *
+     * @return the ledger.
+     */
+    @GetMapping("/results/ledger")
+    public String ledger() {
+        return InfluxV3QueryExecutionMetricsLogger.GetLedger().toString();
+    }
+
+    /**
+     * Return a list of events at a higher level than INFO.
+     * @return
+     */
+    @GetMapping("/results/log")
+    public String log() {
+        return InfluxV3QueryExecutionMetricsLogger.GetLoggingEvents()
+            .stream()
+            .filter(e -> e.getLevel().levelInt > Level.INFO.levelInt)
+            .map(event -> {
+                    return event.getInstant()
+                        + "[" + event.getLoggerName()
+                        + "]: " + event + "\n";
+            }).toList() + "\n";
+    }
+
+    /**
+     * Shutdown the SpringApplication.
+     *
+     * <p>N.B. property <code>server.shutdown=immediate</code> can cause memory leaks.</p>
+     */
+    @GetMapping("/shutdown")
+    public void shutdown() {
+        SpringApplication.exit(applicationContext);
+        log.info("Shutdown type {}", shutdownType);
+    }
+
+    /**
      * Dummy method to simulate processing of point values.
      *
      * @param pointValues the point values to process
@@ -43,6 +158,7 @@ public class QueryController {
      */
     private PointValues buildData(PointValues pointValues) {
         log.debug("building point values: {}", pointValues);
+   //     memoryHog.add(pointValues);
         return pointValues;
     }
 }
--- src/main/java/com/example/influxretry/influx/InfluxV3QueryExecutionMetricsLogger.java
+++ src/main/java/com/example/influxretry/influx/InfluxV3QueryExecutionMetricsLogger.java
@@ -1,8 +1,14 @@
 package com.example.influxretry.influx;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import com.example.influxretry.metrics.Metrics;
 import lombok.experimental.Delegate;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import com.influxdb.v3.client.InfluxDBClient;
 import com.influxdb.v3.client.PointValues;
 import com.influxdb.v3.client.query.QueryOptions;
+import org.slf4j.LoggerFactory;
 
 /**
  * Wrapper around {@link InfluxDBClient} that logs execution metrics for queryPoints(...) calls.
@@ -21,6 +28,15 @@ import com.influxdb.v3.client.query.QueryOptions;
  */
 @Slf4j
 public class InfluxV3QueryExecutionMetricsLogger implements InfluxDBClient {
+
+    static List<String> ResultsLedger = new ArrayList<>();
+    private static final ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+    private static final Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+    static {
+        rootLogger.addAppender(listAppender);
+        listAppender.start();
+    }
 
     @Delegate
     private final InfluxDBClient influxDBClient;
@@ -42,11 +58,21 @@ public class InfluxV3QueryExecutionMetricsLogger implements InfluxDBClient {
         } finally {
             long elapsedTime = System.currentTimeMillis() - startTime;
             log.info("Time to execute query: {} is {} ms and status:{}", query, elapsedTime, status);
+
+            ResultsLedger.add(Instant.now() + ": " + status + "\n");
         }
     }
 
     public void close() throws Exception {
         log.info("Closing InfluxDBClient");
         influxDBClient.close();
+    }
+
+    public static List<String> GetLedger(){
+        return ResultsLedger;
+    }
+
+    public static List<ILoggingEvent> GetLoggingEvents(){
+        return listAppender.list;
     }
 }
